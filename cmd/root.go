7fce7ff2fc663e19a6ec3159282df9d7b328bce6
luphord <luphord@protonmail.com>
2019-06-03 17:12:46 +0200

    update cli description

--- cmd/root.go
+++ cmd/root.go
@@ -12,13 +12,10 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
         Use:   "algoristic",
-        Short: "A brief description of your application",
-        Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+        Short: "A playground for algorithmic art.",
+        Long: `algoristic is going to be a collection of different algorithms
+to create art algorithmically. Each algorithm will be available
+as a subcommand.`,
         // Uncomment the following line if your bare application
         // has an action associated with it:
         //      Run: func(cmd *cobra.Command, args []string) { },

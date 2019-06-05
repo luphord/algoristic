b9256d94448ee136b3ee6114527f9b60be79c58f
luphord <luphord@protonmail.com>
2019-06-05 17:25:55 +0200

    random iteration

--- goldensquares/goldensquares.go
+++ goldensquares/goldensquares.go
@@ -3,8 +3,9 @@ package goldensquares
 import (
 	"io"
 	"math"
+	"math/rand"
 
-	"github.com/ajstarks/svgo"
+	svg "github.com/ajstarks/svgo"
 )
 
 var GoldenRatio = (1 + math.Sqrt(5)) / 2
@@ -14,5 +15,34 @@ func GoldenSquares(writer io.Writer, width uint32) {
 	height := int32(float64(width) / GoldenRatio)
 	canvas.Start(int(width), int(height))
 	canvas.Rect(0, 0, int(width), int(height), "fill:grey")
+	gen := &goldenSquareGenerator{&goldenSquare{0, 0, 0, 0}}
+	for iter := gen; iter.HasNext(); {
+		iter.Next()
+	}
 	canvas.End()
+}
+
+type goldenSquare struct {
+	X      int
+	Y      int
+	Width  int
+	Height int
+}
+
+type goldenSquareGenerator struct {
+	next *goldenSquare
+}
+
+func (gen *goldenSquareGenerator) HasNext() bool {
+	return gen.next != nil
+}
+
+func (gen *goldenSquareGenerator) Next() *goldenSquare {
+	r := rand.Float64()
+	if r < 0.3 {
+		gen.next = nil
+	} else {
+		gen.next = &goldenSquare{1, 2, 3, 4}
+	}
+	return gen.next
 }
